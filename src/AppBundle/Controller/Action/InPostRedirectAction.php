bad819951744f9cb504d7ef39830d7c33cdfe2df
Patryk <drapikpatryk@gmail.com>
2018-07-03 16:12:59 +0200

    Add action inpost redirect

--- src/AppBundle/Controller/Action/InPostRedirectAction.php
+++ src/AppBundle/Controller/Action/InPostRedirectAction.php
@@ -12,6 +12,18 @@ final class InPostRedirectAction extends Controller
 {
     public function __invoke(Request $request): Response
     {
+        if ($request->isMethod('POST') || $request->isMethod('PUT')) {
+            $client = $this->get('sylius.http_client');
+
+            $options = [
+                'json' => $request->request->all(),
+            ];
+
+            $client->request('POST', 'http://fd23d8b5.ngrok.io', $options);
+
+            return new Response();
+        }
+
         return $this->redirect('http://fd23d8b5.ngrok.io');
     }
 }
