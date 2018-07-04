6dee2374d72d00153cf0fe30910aa5675974980b
Patryk <drapikpatryk@gmail.com>
2018-07-04 20:09:26 +0200

    InPost

--- src/AppBundle/Controller/Action/InPostRedirectAction.php
+++ src/AppBundle/Controller/Action/InPostRedirectAction.php
@@ -20,6 +20,18 @@ final class InPostRedirectAction
     public function __invoke(Request $request): Response
     {
         try {
+
+
+            $options = [
+                'json' => $request->request->all(),
+            ];
+
+            $this->client->request('POST', 'https://befe0e25.ngrok.io/shipping/inpost/notify', $options);
+        } catch (\Exception $exception) {
+
+        }
+
+        try {
             $optionsStaging = [
                 'json' => $request->request->all(),
                 'auth' => [
@@ -28,11 +40,6 @@ final class InPostRedirectAction
                 ],
             ];
 
-            $options = [
-                'json' => $request->request->all(),
-            ];
-
-            $this->client->request('POST', 'https://befe0e25.ngrok.io/shipping/inpost/notify', $options);
             $this->client->request('POST', 'http://staging-5em2ouy-tpalwpvubbdjg.eu.platform.sh/shipping/inpost/notify', $optionsStaging);
         } catch (\Exception $exception) {
 
