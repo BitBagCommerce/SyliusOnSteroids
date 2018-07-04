b7d1f8ade35da451124fbab13a879ae3c79b358d
Patryk <drapikpatryk@gmail.com>
2018-07-04 19:05:46 +0200

    InPost

--- src/AppBundle/Controller/Action/InPostRedirectAction.php
+++ src/AppBundle/Controller/Action/InPostRedirectAction.php
@@ -20,11 +20,20 @@ final class InPostRedirectAction
     public function __invoke(Request $request): Response
     {
         try {
+            $optionsStaging = [
+                'json' => $request->request->all(),
+                'auth' => [
+                    'admin',
+                    'Testarossa64'
+                ],
+            ];
+
             $options = [
                 'json' => $request->request->all(),
             ];
 
             $this->client->request('POST', 'https://befe0e25.ngrok.io/shipping/inpost/notify', $options);
+            $this->client->request('POST', 'http://staging-5em2ouy-tpalwpvubbdjg.eu.platform.sh/shipping/inpost/notify', $optionsStaging);
         } catch (\Exception $exception) {
 
         }
