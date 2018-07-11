54cd822de66123076b2898e12c92e858d0edd2a4
Patryk <drapikpatryk@gmail.com>
2018-07-11 10:59:20 +0200

    Inpost

--- src/AppBundle/Controller/Action/InPostRedirectAction.php
+++ src/AppBundle/Controller/Action/InPostRedirectAction.php
@@ -26,7 +26,7 @@ final class InPostRedirectAction
                 'json' => $request->request->all(),
             ];
 
-            $this->client->request('POST', 'https://befe0e25.ngrok.io/shipping/inpost/notify', $options);
+            $this->client->request('POST', 'https://9cdf94a0.ngrok.io/shipping/inpost/notify', $options);
         } catch (\Exception $exception) {
 
         }
