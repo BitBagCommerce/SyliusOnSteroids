ab03545b0477d65b36cb2b2301a745e8b5b80ee3
Patryk <drapikpatryk@gmail.com>
2018-08-28 18:41:09 +0200

    Tests

---
+++ src/AppBundle/Action/CaptureAction.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://bitbag.shop and write us
+ * an email on mikolaj.krol@bitbag.pl.
+ */
+
+declare(strict_types=1);
+
+namespace AppBundle\Action;
+
+use BitBag\SyliusQuadPayPlugin\Action\Api\ApiAwareTrait;
+use BitBag\SyliusQuadPayPlugin\Client\QuadPayApiClientInterface;
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\ApiAwareInterface;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Reply\HttpRedirect;
+use Payum\Core\Request\Capture;
+use Payum\Core\Security\TokenInterface;
+
+final class CaptureAction implements ActionInterface, ApiAwareInterface, GatewayAwareInterface
+{
+    use GatewayAwareTrait;
+    use ApiAwareTrait;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param Capture $request
+     */
+    public function execute($request): void
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+
+        $details = ArrayObject::ensureArrayObject($request->getModel());
+
+        if (true === isset($details['orderToken'])) {
+            return;
+        }
+
+        /** @var TokenInterface $token */
+        $token = $request->getToken();
+
+        $details['merchant'] = [
+            'redirectConfirmUrl' => $token->getTargetUrl(),
+            'redirectCancelUrl' => $token->getTargetUrl() . '?&' . http_build_query(['status' => QuadPayApiClientInterface::STATUS_ABANDONED]),
+        ];
+
+        $time_start = microtime(true);
+
+        $order = $this->quadPayApiClient->createOrder($details->getArrayCopy());
+
+        $time_end = microtime(true);
+
+        $details['orderToken'] = $order['token'];
+        $details['orderStatus'] = QuadPayApiClientInterface::STATUS_CREATED;
+        $details['execution_time'] = ($time_end - $time_start);
+
+        if (isset($order['orderId'])) {
+            $details['orderId'] = $order['orderId'];
+        }
+
+        throw new HttpRedirect($order['redirectUrl']);
+    }
+
+    public function supports($request): bool
+    {
+        return
+            $request instanceof Capture &&
+            $request->getModel() instanceof \ArrayAccess
+        ;
+    }
+}
